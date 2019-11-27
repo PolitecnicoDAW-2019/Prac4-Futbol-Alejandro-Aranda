8f0e4ac8090df321c834b92f6007ecac0aec744a
zuyk <alexarnd99@gmail.com>
2019-11-27 17:35:07 +0100

    feat(Player.model.js): Added constructor

--- src/models/Player.model.js
+++ src/models/Player.model.js
@@ -1,3 +1,12 @@
 class Player {
-  constructor() {}
+  constructor({ id, name, alias, club, birthdate, selectPosition, idPosition, picture }) {
+    this.id = id;
+    this.name = name;
+    this.alias = alias;
+    this.club = club;
+    this.birthdate = birthdate;
+    this.selectPosition = selectPosition;
+    this.picture = picture;
+    this.idPosition = idPosition;
+  }
 }
